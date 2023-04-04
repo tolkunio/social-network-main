6ae0d15fa933428137c5cc2d99f1d2931733d15c
Omurbekova Tolkun <toprog27@gmail.com>
2023-04-04 16:23:25 +0600

    -added dialog page

---
+++ src/Components/Dialogs/Dialogs.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from '../Dialogs/Dialogs.module.css';
+
+const Dialogs = () => {
+    return (
+        <div className={s.content}>
+         <h2>Dialogs page</h2>
+        </div>
+    );
+};
+
+export default Dialogs;
